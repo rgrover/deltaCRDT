fc9ba07cc674eb9bba392ee8ee569f38cc2f6a63
Rohit Grover <rohit@mailbox.org>
2018-12-09 21:54:39 +1300

    trivial test case

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,13 @@
+import           Test.QuickCheck
+
+gen :: Gen (Int, String)
+gen = do
+    i <- arbitrary
+    s <- elements ["rohit", "Grover", "test"]
+    return (i,s)
+
+test :: Int -> Int -> Bool
+test a b = a + b == b + a
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = quickCheck test
