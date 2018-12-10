ecf228c72f8d0edcacbcd8036cf990b0fb37bd71
Rohit Grover <rohit@mailbox.org>
2018-12-10 17:46:36 +1300

    export AggregateState from CRDT.hs

--- src/CRDT/CRDT.hs
+++ src/CRDT/CRDT.hs
@@ -1,5 +1,7 @@
 module CRDT.CRDT
     ( module CRDT.Core.Algorithms
+    , module CRDT.Core.AggregateState
     ) where
 
+import           CRDT.Core.AggregateState
 import           CRDT.Core.Algorithms
